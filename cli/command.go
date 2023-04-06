a61f6b82611c59aac12445625fa3c02a4ff5b41b
chx2333 <1575197604@qq.com>
2023-04-06 22:53:55 +0800

    fix

--- cli/command.go
+++ cli/command.go
@@ -14,19 +14,15 @@ func (cli *CLI) printChain() {
 
 	for {
 		b := bci.Next()
-
-		fmt.Printf("Prev hash: %x\n", b.PrevBlockHash)
-		fmt.Printf("Data: %s\n", b.Data)
-		fmt.Printf("Hash: %x\n", b.Hash)
 		fmt.Printf("|=================================================================================|\n")
 		fmt.Printf("|%-90s|\n", color.YellowString("[PreBlockHash]: %s", hex.EncodeToString(b.PrevBlockHash)))
 		fmt.Printf("|%-90s|\n", color.RedString("[Data]: %x", b.Data))
 		fmt.Printf("|%-90s|\n", color.BlueString("[Timestamp]: %d %v", b.Timestamp, time.Unix(b.Timestamp, 0)))
 		fmt.Printf("|%-90s|\n", color.BlackString("[Hash]: %x", b.Hash))
-		fmt.Println("|=================================================================================|")
-		fmt.Printf("\n\n")
 		pow := block.NewProofOfWork(b)
 		fmt.Printf("|%-90s|\n", color.GreenString("[Block Pow]: %s", strconv.FormatBool(pow.Validate())))
+		fmt.Println("|=================================================================================|")
+
 		fmt.Println()
 
 		if len(b.PrevBlockHash) == 0 {
