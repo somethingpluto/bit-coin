899fd08ccdf0a56379ccea09e9e955d9e3e07fd7
chx2333 <1575197604@qq.com>
2023-04-07 22:10:44 +0800

    fix

--- cli/cli.go
+++ cli/cli.go
@@ -2,67 +2,67 @@ package cli
 
 import (
 	"bitcoin/block"
-	"flag"
+	"bufio"
 	"fmt"
-	"log"
+	"github.com/fatih/color"
 	"os"
+	"strings"
 )
+
+const shell = ">>"
 
 type CLI struct {
 	blockChain *block.BlockChain
 }
 
 const USAGE = `
-	Usage:
- 		addblock -data BLOCK_data  add a block to blockchain(向区块链中添加一个区块 简称挖矿 eg: addblock -data 11)
-		printChain 				   print all block of the chain(打印区块链中所有区块)
+Usage:
+	addblock -data BLOCK_data  add a block to blockchain(向区块链中添加一个区块 简称挖矿 eg: addblock -data 11)
+ 	printChain 				   print all block of the chain(打印区块链中所有区块)
 `
 
+// NewCli
+// @Description: 根据区块链创建cli
+// @param bc
+// @return *CLI
 func NewCli(bc *block.BlockChain) *CLI {
 	return &CLI{blockChain: bc}
 }
 
+// printUsage
+// @Description: 打印cli使用方法
+// @receiver cli
 func (cli *CLI) printUsage() {
 	fmt.Println(USAGE)
 }
 
-func (cli *CLI) validateArgs() {
-	if len(os.Args) < 2 {
-		cli.printUsage()
-		os.Exit(1)
-	}
-}
-
 func (cli *CLI) Run() {
-	cli.validateArgs()
-
-	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
-	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
-	addBlockData := addBlockCmd.String("data", "", "Block data")
-	switch os.Args[1] {
-	case "addblock":
-		err := addBlockCmd.Parse(os.Args[2:])
-		if err != nil {
-			log.Panic(err)
+	cli.printUsage()
+	option := ""
+	args := []string{}
+	for true {
+		fmt.Printf(color.GreenString("%s", shell))
+		reader := bufio.NewReader(os.Stdin)
+		option, _ = reader.ReadString('\n')
+		args = strings.Split(option, " ")
+		fmt.Println(args)
+		switch args[0] {
+		case "printchain":
+			cli.printChain()
+			break
+		case "addblock":
+			if args[1] != "data" || args[2] == "" {
+				fmt.Println("addblock command invalid")
+				cli.printUsage()
+				break
+			} else {
+				cli.blockChain.AddBlock(args[2])
+			}
+		case "quit":
+			return
+		default:
+			fmt.Println("invalid input")
+			fmt.Println(USAGE)
 		}
-	case "printChain":
-		err := printChainCmd.Parse(os.Args[2:])
-		if err != nil {
-			log.Panic(err)
-		}
-	default:
-		cli.printUsage()
-		os.Exit(1)
-	}
-	if addBlockCmd.Parsed() {
-		if *addBlockData == "" {
-			addBlockCmd.Usage()
-			os.Exit(1)
-		}
-		cli.blockChain.AddBlock(*addBlockData)
-	}
-
-	if printChainCmd.Parsed() {
-		cli.printChain()
 	}
 }
